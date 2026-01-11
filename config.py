c4478ea6c04c9986241eab80966a090da9c9d785
Harshasbiabwinwi <harshvardhanprogamingi@gmail.com>
2026-01-11 14:42:35 +0530

    Set default FORCE_SUB_CHANNEL value

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://villainravangaming:mikey_
 DB_NAME = os.environ.get("DATABASE_NAME", "villainravangaming")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1003433236081"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
